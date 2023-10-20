3df3c4a72a85b5b67eb9d41f6aed76c219875ff3
BobbyH49 <robert.horrocks@sky.com>
2023-10-20 23:46:45 +0100

    Linux Server has to be created post domain join

--- scripts/DeploySQL.ps1
+++ scripts/DeploySQL.ps1
@@ -12,31 +12,31 @@ Set-Item -Path Env:\SuppressAzurePowerShellBreakingChangeWarnings -Value $true
 Write-Host "Connecting to Azure"
 Connect-AzAccount -Identity | out-null
 
-#    # Deploy Linux Server with public key authentication
-#    Write-Header "Deploying Linux Server with public key authentication"
+# Deploy Linux Server with public key authentication
+Write-Header "Deploying Linux Server with public key authentication"
 
-#    # Generate ssh keys
-#    Write-Host "Generating ssh keys"
-#    $linuxKeyFile = $Env:linuxVM.ToLower() + "_id_rsa"
-#    mkdir C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh
-#    ssh-keygen -q -t rsa -b 4096 -N '""' -f C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\$linuxKeyFile
-#    $publicKey = Get-Content C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\$linuxKeyFile.pub
+# Generate ssh keys
+Write-Host "Generating ssh keys"
+$linuxKeyFile = $Env:linuxVM.ToLower() + "_id_rsa"
+mkdir C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh
+ssh-keygen -q -t rsa -b 4096 -N '""' -f C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\$linuxKeyFile
+$publicKey = Get-Content C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\$linuxKeyFile.pub
 
-#    # Generate parameters for template deployment
-#    Write-Host "Generating parameters for template deployment"
-#    $templateParameters = @{}
-#    $templateParameters.add("adminUsername", $Env:adminUsername)
-#    $templateParameters.add("sshRSAPublicKey", $publicKey)
-#    $templateParameters.add("vnetName", $Env:vnetName)
-#    $templateParameters.add("linuxVM", $Env:linuxVM)
+# Generate parameters for template deployment
+Write-Host "Generating parameters for template deployment"
+$templateParameters = @{}
+$templateParameters.add("adminUsername", $Env:adminUsername)
+$templateParameters.add("sshRSAPublicKey", $publicKey)
+$templateParameters.add("vnetName", $Env:vnetName)
+$templateParameters.add("linuxVM", $Env:linuxVM)
 
-#    # Deploy Linux server
-#    Write-Host "Deploying $Env:linuxVM"
-#    New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroup -Mode Incremental -Force -TemplateFile "C:\Deployment\templates\linux.json" -TemplateParameterObject $templateParameters
+# Deploy Linux server
+Write-Host "Deploying $Env:linuxVM"
+New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroup -Mode Incremental -Force -TemplateFile "C:\Deployment\templates\linux.json" -TemplateParameterObject $templateParameters
 
-#    # Add known host
-#    Write-Host "Adding $Env:linuxVM as known host"
-#    ssh-keyscan -t rsa 10.$Env:vnetIpAddressRangeStr.16.5 >> C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\known_hosts
+# Add known host
+Write-Host "Adding $Env:linuxVM as known host"
+ssh-keyscan -t rsa 10.$Env:vnetIpAddressRangeStr.16.5 >> C:\Users\$Env:adminUsername.$Env:netbiosName\.ssh\known_hosts
 
 #ssh -i C:\Users\azureuser.SQLK8s\.ssh\sqlk8slinux_id_rsa azureuser@10.192.16.5
 
